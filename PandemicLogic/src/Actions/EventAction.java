4c01a7ec69b7704596eba8320d7fe075e4f4552c
Bao NGUYEN <bnguye31@ds2.CS.McGill.CA>
2018-04-02 13:24:56 -0400

    Minor changes to EventAction

--- PandemicLogic/src/Actions/EventAction.java
+++ PandemicLogic/src/Actions/EventAction.java
@@ -6,18 +6,33 @@ public class EventAction implements Action{
 	private static ArrayList<Event> EventQueue = initializeEventQueue();
 	@Override
 	public void execute(String[] args) {
-		
+		for (Event e: EventQueue){
+			if (e.getName().equals(args[2])){
+				e.executeEvent(args);
+			}
+		}
 	}
 
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return "Event";
+		return "EventAction";
 	}	
 	private static ArrayList<Event> initializeEventQueue(){
 		ArrayList<Event> toReturn = new ArrayList<Event>();
-		Event aaa = new BorrowedTime();
-		toReturn.add(aaa);
+		toReturn.add(new Airlift());
+		toReturn.add(new BorrowedTime());
+		toReturn.add(new CommercialTravelBan());
+		toReturn.add(new Forecast());
+		toReturn.add(new GovernmentGrant());
+		toReturn.add(new MobileHospital());
+		toReturn.add(new NewAssignment());
+		toReturn.add(new OneQuietNight());
+		toReturn.add(new RapidVaccineDeployment());
+		toReturn.add(new ReexaminedResearch());
+		toReturn.add(new RemoteTreatment());
+		toReturn.add(new ResilientPopulation());
+		toReturn.add(new SpecialOrders());
 		return toReturn;
 		
 	}
